e7816145344858390bf4f739f31e01a72f4c3ae4
Ryan Zayne <phoenixastro97@gmail.com>
2022-09-25 14:50:02 +0100

    update

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
 let generateEl = document.getElementById("generate-el");
 let passwordBoxOne = document.getElementById("password-el-1");
 let passwordBoxTwo = document.getElementById("password-el-2");
-let passwordLength = 10;
+let passwordBoxArr = [passwordBoxOne, passwordBoxTwo]
+let passwordLength = 8;
 
-function generateRandomChar(){
+function generateRandomChar() {
     let randomChar = Math.floor(Math.random() * characters.length);
     return characters[randomChar];
 }
 
-generateEl.addEventListener("click", passwordLimit)
-generateEl.addEventListener("click", generateRandomPassword);
+generateEl.addEventListener("click", passwordLimit);
 
-
-function passwordLimit(){
+function passwordLimit() {
     let passwordLengthx = document.getElementById("password").value;
 
-    if (passwordLengthx === "" || passwordLengthx < 1){
-        passwordLength = 10;
+    if (passwordLengthx === "" || passwordLengthx < 1) {
+        passwordLength = 8;
     } else {
         passwordLength = passwordLengthx;
     }
-    
+
 }
 
-function generateRandomPassword(){
+
+generateEl.addEventListener("click", generateRandomPassword);
+
+function generateRandomPassword() {
     let randomPasswordOne = " ";
-    
-    
-    for (i = 0; i < passwordLength; i++){
+
+
+    for (let i = 0; i < passwordLength; i++) {
         randomPasswordOne += generateRandomChar();
     }
 
     let randomPasswordTwo = " ";
-    for (i = 0; i < passwordLength; i++){
+    for (let i = 0; i < passwordLength; i++) {
         randomPasswordTwo += generateRandomChar();
     }
     passwordBoxOne.textContent = randomPasswordOne;
     passwordBoxTwo.textContent = randomPasswordTwo;
-    
+
 }
 
 
+passwordBoxArr.forEach((box) => box.addEventListener("click", copyToClipboard))
 
+function copyToClipboard(str) {
+    const targetBox = str.target;
+
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        return navigator.clipboard.writeText(targetBox.textContent)
+    } else {
+        return Promise.reject('The Clipboard API is not available.');
+    }
+
+}
