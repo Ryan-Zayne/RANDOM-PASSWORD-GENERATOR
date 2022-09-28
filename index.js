2cb249de6ecd5d62809878abb2dd99a90a7bb712
Ryan Zayne <phoenixastro97@gmail.com>
2022-09-28 18:21:29 +0100

    .

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
 const generateBtn = document.getElementById("generate-btn");
 const passwordBoxOne = document.getElementById("password-box-1").firstElementChild;
 const passwordBoxTwo = document.getElementById("password-box-2").firstElementChild;
 const copyBtn = document.querySelectorAll(".copy-btn");
 const passwordSlider = document.getElementById("password-range");
-const toolTip = document.querySelector('.tooltip')
+const toolTip = document.querySelector(".tooltip");
 let passwordLength = 8;
 
 // All event listeners
@@ -37,8 +36,8 @@ function passwordLimit() {
 
 // Generates and outputs the password in the DOM
 function generateRandomPassword() {
-    let randomPasswordOne = " ";
-    let randomPasswordTwo = " ";
+    let randomPasswordOne;
+    let randomPasswordTwo;
 
     for (let i = 0; i < passwordLength; i++) {
         randomPasswordOne += generateRandomChar();
@@ -75,5 +74,3 @@ function copyToClipboard(e) {
         }, 1200);
     }
 }
-
-
