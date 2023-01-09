8ba3dfdee0e9e6d7e31911888228d7b023148642
Ryan Zayne <93886198+Ryan-Zayne@users.noreply.github.com>
2023-01-09 07:58:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 
 const generateBtn = document.getElementById('generate-btn');
 const passwordBoxOne = document.getElementById('password-box-1').firstElementChild;
@@ -20,6 +19,7 @@ copyBtn.forEach((btn) => btn.addEventListener('click', copyToClipboard));
 
 // Generates random characters
 function generateRandomChar() {
+	const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
 	const randomChar = Math.floor(Math.random() * characters.length);
 	return characters[randomChar];
 }
